b1bc05ad46011ca87356b505654a6db91cb3c162
Jonathan Darwin <jonathanjojo38@yahoo.com>
2020-06-15 13:59:17 +0700

    adding remove product from cart functionality

--- src/components/cart/cart.jsx
+++ src/components/cart/cart.jsx
@@ -1,23 +1,36 @@
 import React, { Component } from 'react';
+import { useDispatch } from 'react-redux'
 
-function Cart({cartList}) {      
+function Cart({cartList}) {         
+    const dispatch = useDispatch()
+
     return (  
         cartList.map(cart => {            
             return (                                
                 <div className="row" key={cart.id}>
-                    <div className="col-md-2">
+                    <div className="col-md-1">                        
+                        <i className="fa fa-minus-circle ic-delete" aria-hidden="true" onClick={() => dispatch(decrementCart(cart.id))}></i>
+                    </div>
+                    <div className="col-md-1">
                         <span>{cart.qty}x</span>
                     </div>
                     <div className="col-md-5">
                         <strong>{cart.name}</strong>
                     </div>
-                    <div className="col-md-5">
+                    <div className="col-md-4">
                         <span>{cart.price}</span>
                     </div>
                 </div>                                 
             )  
         })
     );
+}
+
+const decrementCart = id => {    
+    return {
+        type : "DECREMENT_CART",
+        id : id
+    }
 }
  
 export default Cart
--- src/components/cart/cartReducer.js
+++ src/components/cart/cartReducer.js
@@ -1,27 +1,55 @@
-const CartReducer = (state = [], action) => {
+const CartReducer = (state = [], action) => {    
     switch(action.type){
-        case "ADD" :                        
-            const temp = [...state];            
-            const search = temp.filter(obj => {
-                return obj.id === action.value.id
-            })                        
+        case "ADD" :                         
+            console.log('add cart')
+            return addCart(state, action)            
 
-            if(search.length == 0){                
-                temp.push({
-                    id: action.value.id,
-                    name: action.value.name,
-                    price: action.value.price * action.qty,
-                    qty: action.qty
-                });
-            }
-            else{
-                const idx = temp.indexOf(search[0])                                
-                temp[idx].qty += action.qty
-                temp[idx].price += (action.value.price * action.qty)
-            }                        
-            return temp;            
+        case "DECREMENT_CART" :
+            console.log('decrement cart')
+            return decrementCart(state, action)            
     }    
     return state;
+}
+
+const addCart = (state, action) => {
+    let temp = [...state]      
+    const search = temp.filter(obj => {
+        return obj.id === action.value.id
+    })                        
+
+    if(search.length == 0){                
+        temp.push({
+            id: action.value.id,
+            name: action.value.name,
+            price: action.value.price * action.qty,
+            qty: action.qty
+        });
+    }
+    else{
+        const idx = temp.indexOf(search[0])                                
+        temp[idx].qty += action.qty
+        temp[idx].price += (action.value.price * action.qty)
+    }                        
+    return temp;    
+}
+
+const decrementCart = (state, action) => {
+    const id = action.id            
+    let temp = [...state]      
+    const search = temp.filter(e => e.id === id)
+
+    if(search.length == 1){
+        const idx = temp.indexOf(search[0])                
+        if(temp[idx].qty == 1){
+            temp = temp.filter(e => e.id != id)                    
+        }
+        else{
+            let originalPrice = temp[idx].price / temp[idx].qty
+            temp[idx].qty--;
+            temp[idx].price -= originalPrice;                    
+        }                
+    }
+    return temp;
 }
 
 export default CartReducer
